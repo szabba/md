961559f70eb4b101b96ec342dbae63256dfbe8d4
Karol Marcjan <karol.marcjan@gmail.com>
2014-03-26 12:59:57 +0100

    Add Euler integrator

--- src/shm/main.go
+++ src/shm/main.go
@@ -1,4 +1,27 @@
 package main
 
+func Shift(xs []float64, x float64) {
+
+	for i := len(xs) - 1; i > 0; i-- {
+
+		xs[i] = xs[i-1]
+	}
+
+	xs[0] = x
+}
+
+type Integrator func(xs, vs []float64, a, dt float64)
+
+func Euler(xs, vs []float64, a, dt float64) {
+
+	v := vs[0] + dt*a
+	x := xs[0] + dt*v
+
+	Shift(vs, v)
+	Shift(xs, x)
+
+	return
+}
+
 func main() {
 }
