c4d555817c4566ac51471b8f10769c40b1835dc9
Karol Marcjan <karol.marcjan@gmail.com>
2014-03-27 19:38:48 +0100

    Update build script

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
-go fmt ./src/shm
-go test ./src/shm
-go build ./src/shm && ./shm > data.dat
+for pkg in vect newton shm; do
+	go fmt "./src/$pkg"
+	go test "./src/$pkg"
+done
+
+go build ./src/shm && ./shm > shm.dat
