f447c0266a07b5cf9cea8d3ef3d7306176ca7e2f
Karol Marcjan <karol.marcjan@gmail.com>
2014-03-28 22:51:22 +0100

    Add newton.NewBody

--- src/newton/body.go
+++ src/newton/body.go
@@ -11,6 +11,21 @@ import (
 type Body struct {
 	Xs, Vs []vect.Vector
 	M      float64
+	currAt int
+}
+
+// Constructs a body of specified mass suitable for working with the integrator
+func NewBody(algo Integrator, mass float64) *Body {
+
+	b := new(Body)
+
+	b.Xs = make([]vect.Vector, algo.StateLen())
+	b.Vs = make([]vect.Vector, algo.StateLen())
+
+	b.M = mass
+	b.currAt = algo.CurrentAt()
+
+	return b
 }
 
 // Shifts all the values in xs by one and puts x at the beginning.
